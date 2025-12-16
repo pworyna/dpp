5944a3042021876414ae5097a65f78fdd9ecabee
pworyna <147373367+pworyna@users.noreply.github.com>
2025-12-16 21:56:28 +0100

    Delete database.py

--- database.py
+++
@@ -1,7 +0,0 @@
-from sqlalchemy import create_engine
-from sqlalchemy.orm import sessionmaker, declarative_base
-
-engine = create_engine("sqlite:///movies.db", echo=False)
-Session = sessionmaker(bind=engine)
-Base = declarative_base()
-session = Session()
